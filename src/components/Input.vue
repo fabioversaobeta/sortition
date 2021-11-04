d7a23683b7d94d30857a18fedcdfc74d3a6a7359
Fabio Bandacheski <fabio.versao.beta@gmail.com>
2021-11-04 13:13:47 -0300

    input component

---
+++ src/components/Input.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <v-text-field v-if="typeInput == 'required'"
+      :label="label"
+      :rules="rules"
+      hide-details="auto"
+      v-model="valueField"
+    ></v-text-field>
+
+    <v-text-field v-else-if="typeInput == 'normal'" 
+        label="Another input" 
+        v-model="valueField"
+    ></v-text-field>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Input',
+    props: {
+        label: String,
+        typeInput: String,
+    },
+    data() {
+        return {
+            rules: [
+                value => !!value || 'Obrigatório.',
+                value => (value && value.length >= 3) || 'Min. 3 letras',
+            ],
+            valueField: '',
+        }
+    },
+}
+</script>
